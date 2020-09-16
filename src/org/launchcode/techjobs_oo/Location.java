bfd8280e705ab8a52cc46dff8f521201ec22f6dd
Austin Thomas <thomasaustin78@gmail.com>
2020-09-16 09:35:31 -0500

    finished setting up setters and getters

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -14,7 +14,17 @@ public class Location {
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
+    public int attainId() {
+        return id;
+    }
 
+    public String attainValue() {
+        return value;
+    }
+
+    public void assignValue(String value) {
+        this.value = value;
+    }
 
     // Custom toString, equals, and hashCode methods:
 
